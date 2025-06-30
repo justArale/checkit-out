7930ff98f2c906838a576aef8ec4c708658bd21a
justArale <just1arale@gmail.com>
2025-06-30 22:31:40 +0200

    Add missing border-radius to the shoppingItemList class, styling empty list component

--- frontend/src/components/ShoppingitemList.tsx
+++ frontend/src/components/ShoppingitemList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { ShoppingItemType } from "../interfaces/shoppingItem.interface";
 import ShoppingItemCard from "./ShoppingItemCard";
+import EmptyList from "../assets/icon/EmptyList.svg";
 
 interface Props {
   items: ShoppingItemType[];
@@ -8,7 +9,12 @@ interface Props {
 }
 const ShoppingItemList: React.FC<Props> = ({ items, onItemChanged }) => {
   if (items.length === 0) {
-    return <div>Your list is empty</div>;
+    return (
+      <div className="shoppingItemList">
+        <img src={EmptyList} alt="Empty List Icon" />
+        <p className="bodyfontMedium secondaryFontColor">Your list is empty</p>
+      </div>
+    );
   }
 
   return (
