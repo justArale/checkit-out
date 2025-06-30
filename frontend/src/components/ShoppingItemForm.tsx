3930840f96a6ef33c6837cab68f36b7ddf69aabf
justArale <just1arale@gmail.com>
2025-06-30 15:55:39 +0200

    Setup component files

---
+++ frontend/src/components/ShoppingItemForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShoppingItemForm: React.FC = () => {
+  return <div>ShoppingItemForm</div>;
+};
+
+export default ShoppingItemForm;
---
+++ frontend/src/components/ShoppingitemList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShoppingitemList: React.FC = () => {
+  return <div>ShoppingitemList</div>;
+};
+
+export default ShoppingitemList;
