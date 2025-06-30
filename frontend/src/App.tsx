68aa9d1af01fd97772657ea3b5cc651951daa72f
justArale <just1arale@gmail.com>
2025-06-30 15:03:39 +0200

    Setup functionality to use routes and add route for the dashboardpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import DashboradPage from "./pages/DashboradPage";
+
 function App() {
   return (
     <>
       <h4>Checkit Out!</h4>
+      <Routes>
+        <Route path="/" element={<DashboradPage />} />
+        {/* Add more routes here as needed */}
+      </Routes>
     </>
   );
 }
---
+++ frontend/src/pages/DashboradPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashboradPage: React.FC = () => {
+  return <div>React.FC</div>;
+};
+
+export default DashboradPage;
