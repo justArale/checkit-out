df9b658955e554e0bb6def58af2a95b16b57d43e
justArale <just1arale@gmail.com>
2025-06-30 17:27:03 +0200

    Build ShoppingItemList component adn implement in DashboardPage

--- frontend/src/components/ShoppingitemList.tsx
+++ frontend/src/components/ShoppingitemList.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { getAllShoppingItems } from "../services/shoppingItem.service";
+import type { ShoppingItemType } from "../services/shoppingItem.service";
 
 const ShoppingItemList: React.FC = () => {
-  return <div>ShoppingItemList</div>;
+  const [items, setItems] = useState<ShoppingItemType[]>([]);
+
+  useEffect(() => {
+    getAllShoppingItems()
+      .then((data) => setItems(data.items))
+      .catch((error) => {
+        console.error("Error fetching items:", error);
+      });
+  }, []);
+
+  if (items.length === 0) {
+    return <div>Your list is empty</div>;
+  }
+
+  return (
+    <div>
+      {items.map((item) => (
+        <div key={item._id}>{item.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ShoppingItemList;
--- frontend/src/pages/DashboradPage.tsx
+++ frontend/src/pages/DashboradPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ShoppingItemForm from "../components/ShoppingItemForm";
+import ShoppingItemList from "../components/ShoppingItemList";
 
 const DashboradPage: React.FC = () => {
   return (
     <>
       <ShoppingItemForm />
+      <ShoppingItemList />
     </>
   );
 };
