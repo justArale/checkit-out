1a8c21b7b8b785e9202a80dc50724d646ddbbb25
justArale <just1arale@gmail.com>
2025-06-30 16:04:52 +0200

    Create errorpage-file and add route as wildcard to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import DashboradPage from "./pages/DashboradPage";
+import ErrorPage from "./pages/ErrorPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<DashboradPage />} />
+        <Route path="*" element={<ErrorPage />} />
         {/* Add more routes here as needed */}
       </Routes>
       <Footer />
