81593f8903ff4fdaa973e23563c44c5db375ebb9
justArale <just1arale@gmail.com>
2025-06-30 22:23:55 +0200

    Styling footer: Add svg-icon to replace the heart-emoji, style font and wrapper to align with the design

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import HeartUnfilled from "../assets/icon/HeartUnfilled.svg";
 
 const Footer: React.FC = () => {
-  return <div>Made with ♡ by Arale</div>;
+  return (
+    <div className="footer">
+      <p className="labelfontRegular secondaryFontColor footerInline">
+        Made with{" "}
+        <span>
+          <img src={HeartUnfilled} className="iconWrapper" />
+        </span>{" "}
+        by Arale
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
